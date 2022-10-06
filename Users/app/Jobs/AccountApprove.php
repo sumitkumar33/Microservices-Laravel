3b05e2bf6d5bbd05fee55ba7890b48e451692c3c
Sumit Kumar <sumit.kumar@hestabit.in>
2022-10-06 05:38:16 +0530

    Minor bug fix

--- Users/app/Jobs/AccountApprove.php
+++ Users/app/Jobs/AccountApprove.php
@@ -9,12 +9,17 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class AccountApprove implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
     private $data;
     private $id;
+    public $name;
+    public $email;
+    public $admin_name;
+
     /**
      * Create a new job instance.
      *
@@ -23,6 +28,9 @@ class AccountApprove implements ShouldQueue
     public function __construct($dump, $id)
     {
         $this->data = $dump;
+        $this->name = $dump['name'];
+        $this->email = $dump['email'];
+        $this->admin_name = $dump['admin_name'];
         $this->id = $id;
     }
 
@@ -36,12 +44,14 @@ class AccountApprove implements ShouldQueue
         //Once the student/teacher account has been approved by the admin, trigger a mail to the
         //respective user.
         //Create in app notifications for above 2 cases.
-        Http::post('http://notifications.myproject.com/api/notifications/notify/'.$this->id,
-                $this->data);
+        Http::post(
+            'http://notifications.myproject.com/api/notifications/notify/' . $this->id,
+            $this->data
+        );
         $data2 = [
-            'name' => $this->data['name'],
-            'email' => $this->data['email'],
-            'admin_name' => $this->data['admin_name'],
+            'name' => $this->name,
+            'email' => $this->email,
+            'admin_name' => $this->admin_name,
         ];
         Http::post('http://notifications.myproject.com/api/approveMail', $data2);
     }
